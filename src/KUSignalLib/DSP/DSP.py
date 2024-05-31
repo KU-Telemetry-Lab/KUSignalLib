562f77c8ceeadfa56a5349fd49f0991863d02e56
Luke-Staud1 <lukestaud182@ku.edu>
2024-05-31 12:24:01 -0500

    fices for pull request

--- src/KUSignalLib/DSP/DSP.py
+++ src/KUSignalLib/DSP/DSP.py
@@ -2,18 +2,25 @@ import numpy as np
 
 def IIRDirectForm2(b, a, x):
     """
-    DO NOT TRUST THIS 99.9% sure its wrong
-    Direct Form II IIR filter implementation
-    :param b: numerator coefficients
-    :param a: denominator coefficients
-    :param x: input signal
-    :return: output signal
+    Direct Form II IIR filter implementation.
+    
+    Note: This implementation assumes that the coefficients 'b' and 'a' are ordered such that b[0] and a[0] correspond 
+    to the highest-order coefficient, and b[-1] and a[-1] correspond to the lowest-order coefficient.
+
+    :param b: Numerator coefficients (numpy array)
+    :param a: Denominator coefficients (numpy array)
+    :param x: Input signal
+    :return: Output signal
     """
-    ldfkgn;ldkng
     # Initialize the delay lines
     n = len(b)
     m = len(a)
-    maxLen = max(n, m)
+    if n > m:
+        maxLen = n
+        a = np.concatenate((a, np.zeros(n - m)))
+    else:
+        maxLen = m
+        b = np.concatenate((b, np.zeros(m - n)))
     denominator = a.copy()
     denominator[1:] = -denominator[1:] #flip sign of denominator coefficients
     denominator[0] = 0 #zero out curent p(0) value for multiply, will add this coeff. back in for new x[n] term
