cee7edcf6f25be154088f63bf8de628b46ebd8ae
Luke Staudacher <lukestaud182@ku.edu>
2024-08-01 14:57:09 -0500

    Revert "Merge pull request #46 from KU-Telemetry-Lab/powersthegreat-patch-3"
    This reverts commit d92380056451440dffb7c45db38767700d124fc4, reversing
    changes made to c3b5c4910e219562c2963c4c74f912eceabdd5d0.

--- src/KUSignalLib/__init__.py
+++ src/KUSignalLib/__init__.py
@@ -1,6 +1,6 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 from .examples import examples
 from .communications import communications, SCS
-from .DSP import DSP, PLL
+from .DSP import DSP, PLL, CORDIC
 from .radar import radar
 from .MatLab import MatLab
